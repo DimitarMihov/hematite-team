b6a42f05d054847c3462c1232462e82d2cb3f166
lyuben.hranov@gmail.com <lyuben.hranov@gmail.com@4dc4eff9-4770-9410-b71e-c108e479c7e8>
2013-11-20 15:25:05 +0000

    Base structure for the basic classes created

---
+++ trunk/General/CarInsurance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General
+{
+    public class CarInsurance : AutoService
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public void RemindForExpiration() 
+        {
+            // TODO: Implement the RemindForExpiration() method
+        }
+    }
+}
---
+++ trunk/General/IContactable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General
+{
+    public interface IContactable
+    {
+        void SendEmail();
+        void SendSMS();
+    }
+}
